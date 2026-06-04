6a6869b7e1bba5ac6fba7b970984fbaa4a1a8b6a
Hamzat Rukayat <hamzatrukayat2822@gmail.com>
2026-06-04 15:44:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const dns = require("dns")
+
 
 const connectDB = require("./config/db");
 
 
-dns.setServers(["1.1.1.1", "8.8.8.8"])
+
 
 
 const app = express();
